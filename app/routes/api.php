3c0af0a895f6f66a533f12a5746a96a41cc40c82
Dawid Niemiec <dn305391@student.polsl.pl>
2022-12-08 16:57:18 +0100

    Corrected methods and URLs in routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\PeopleController;
 
 Route::get('niemiec/305391/people', [PeopleController::class, 'index']);
 Route::get('niemiec/305391/people/{people}', [PeopleController::class, 'show']);
-Route::post('niemiec/305391/people/{people}', [PeopleController::class, 'create']);
-Route::post('niemiec/305391/people/{people}', [PeopleController::class, 'update']);
-Route::post('niemiec/305391/people/{people}', [PeopleController::class, 'destroy']);
+Route::post('niemiec/305391/people', [PeopleController::class, 'create']);
+Route::put('niemiec/305391/people/{people}', [PeopleController::class, 'update']);
+Route::delete('niemiec/305391/people/{people}', [PeopleController::class, 'destroy']);
 
