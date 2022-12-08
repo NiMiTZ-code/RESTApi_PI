de349b0b161a75254d554cca503460fd7a07add6
Dawid Niemiec <dn305391@student.polsl.pl>
2022-12-08 17:27:30 +0100

    Added faker dependency and faker generated values to migration

--- app/database/migrations/2022_12_08_161307_create_people_table.php
+++ app/database/migrations/2022_12_08_161307_create_people_table.php
@@ -4,19 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+require_once 'vendor/autoload.php';
 return new class extends Migration
 {
 
     public function up()
     {
+        
         Schema::create('people', function (Blueprint $table) {
+            $faker = Faker\Factory::create();
             $table->increments('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->unsignedInteger('phone_num');
-            $table->string('street');
-            $table->string('city');
-            $table->string('country');
+            $table->string($faker->firstName());
+            $table->string($faker->lastName());
+            $table->unsignedInteger($faker->phoneNumber());
+            $table->string($faker->streetAddress());
+            $table->string($faker->city());
+            $table->string($faker->country());
             $table->timestamps();
         });
     }
