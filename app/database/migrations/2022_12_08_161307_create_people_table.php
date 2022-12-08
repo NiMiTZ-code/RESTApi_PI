53afda5d1223da4642d6322860c22bc1f0b51c45
Dawid Niemiec <dn305391@student.polsl.pl>
2022-12-08 17:16:07 +0100

    Created Migration. Minor changes in controller

---
+++ app/database/migrations/2022_12_08_161307_create_people_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->unsignedInteger('phone_num');
+            $table->string('street');
+            $table->string('city');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
